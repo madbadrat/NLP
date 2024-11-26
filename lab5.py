38ce8d0e7eb8c7e8a3861b143a760eeb74dac958
Slava Vorotov <madbadrat301@yandex.ru>
2024-11-26 22:11:22 +0700

    done lab 5

---
+++ lab5.py
@@ -0,0 +1,28 @@
+import numpy as np
+import torch
+
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+
+np.random.seed(42)
+torch.manual_seed(42)
+
+
+def load_tokenizer_and_model(model_name_or_path):
+    return GPT2Tokenizer.from_pretrained(model_name_or_path), GPT2LMHeadModel.from_pretrained(model_name_or_path)
+
+
+def generate(model, tok, text, do_sample=True, max_length=100, repetition_penalty=5.0, top_k=5, top_p=0.95,
+             temperature=1, num_beams=None, no_repeat_ngram_size=3):
+    input_ids = tok.encode(text, return_tensors="pt")
+    print(model.generate.__globals__['__file__'])
+    out = model.generate(input_ids, max_length=max_length, repetition_penalty=repetition_penalty, do_sample=do_sample,
+                         top_k=top_k, top_p=top_p, temperature=temperature, num_beams=num_beams,
+                         no_repeat_ngram_size=no_repeat_ngram_size)
+    return list(map(tok.decode, out))
+
+
+prompt = 'Грибы - замечательный продукт. Вкусный, полезный, питательный. Их можно приготовить различными способами. Их можно варить, '
+
+tok, model = load_tokenizer_and_model("sberbank-ai/rugpt3large_based_on_gpt2")
+generated = generate(model, tok, prompt, num_beams=10)
+print(generated[0])
